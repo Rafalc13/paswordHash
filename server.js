eb8b6613b06aa6c732fba4388f80fc97f09c05f8
Rafalc13 <seb.baadi@hotmail.com>
2023-12-04 03:11:42 +0000

    gamma test 1

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log('Hash: ' + hash)
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log('True: ' + res)
+  });
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log('False: ' + res)
+  })
+});
 
 
 //END_ASYNC
